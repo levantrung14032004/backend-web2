5109352b3135aa4e44d447e052413463cf65f652
trungnc113 <nguyencongtrung13@gmail.com>
2024-11-28 22:11:17 +0700

    I have added an API to use the cron job service.

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,8 @@
-import * as product from "../controllers/product.js";
-import upload from "../middleware/multer.js";
 import express from "express";
 const route = express.Router();
 
-
-route.get("/api/product", product.handleGetAllProducts);
-route.post("/add-product", upload.array("product", 5), product.add_product);
+route.get("/cron-job", () => {
+  console.log("Cron job is running");
+});
 
 export default route;
