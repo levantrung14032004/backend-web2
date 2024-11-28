1561e38281dc80f1760c60b523568a7dca3243dd
trungnc113 <nguyencongtrung13@gmail.com>
2024-11-28 22:15:14 +0700

    I have added an API to use the cron job service.

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,9 @@
 import express from "express";
 const route = express.Router();
 
-route.get("/cron-job", () => {
+route.get("/cron-job", (req, res) => {
   console.log("Cron job is running");
+  return res.status(200).json({ message: "Cron job is running" });
 });
 
 export default route;
